cfe6fb0de53da1d60830cfda8710d24bef15d172
Martin LAROCHE <martin04.laroche@gmail.com>
2020-10-22 08:44:42 +0200

    ajout page html

--- index.php
+++ index.php
@@ -36,7 +36,8 @@ echo " <pre>
             <div class='mld-card'>
     
                 <img src=".$chemin.$ligne["img"].">"."<br> ".$ligne['nom']."<br> ".$ligne['description']."
-                <input type='submit' name='$cmd' value='Voir'>
+                <br><input type='submit' name='$cmd' value='Voir'>
+
             </div>
         </div>
     </div>
@@ -67,6 +68,7 @@ if(isset($_GET['cmd'])){
                             
 
                     </pre>";
+
                 }
             break; 
         default:
@@ -83,3 +85,20 @@ if(isset($_GET['cmd'])){
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
 </html>
+
+
+
+
+
+
+
+<?php 
+include "inc/config.php";
+include "classes/page.php";
+$ma_page=new Page();
+
+$ma_page->prepare();
+
+echo $ma_page;
+
+?>
